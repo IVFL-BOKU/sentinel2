58d32a784012a7bf465c16fefb01c52d51fb1ef8
Sebastian Boeck <sebastian.boeck@boku.ac.at>
2016-11-09 17:58:45 +0100

    adds function to check coin budget

---
+++ R/S2_user_info.R
@@ -0,0 +1,25 @@
+#' User Info
+#'
+#' Return information on a users coins budget
+#'
+#' @return list where 'userId' is the Id of the current user, 'coins' is the
+#'   total number of coins acquired by the user, 'coinsUsed' the number of coins
+#'   used so far and 'coinsRemain' the number of coins available to acquire
+#'   database products
+#' @export
+
+S2_user_info <- function(){
+  user     <- getOption("S2user")
+  password <- getOption("S2password")
+  srvrsp   <- httr::GET('https://s2.boku.eodc.eu/user/current',
+                        config = httr::authenticate(user, password))
+
+  srvrsp             <- httr::content(srvrsp)
+  srvrsp$coinsRemain <- srvrsp$coins - srvrsp$coinsUsed
+  srvrsp$admin       <- NULL
+
+  return(srvrsp)
+}
+
+# S2_user_info()
+
