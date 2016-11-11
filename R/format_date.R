2cfe7162f56604ac6e9e3778208655ee35cda203
Sebastian Boeck <sebastian.boeck@boku.ac.at>
2016-11-11 12:21:41 +0100

    adds function to reformat date for file naming

---
+++ R/format_date.R
@@ -0,0 +1,13 @@
+#' Reformats date
+#'
+#' Simplifies the 'date' from 'YYYY-MM-DD HH:MM:SS' to 'YYYYMMDD' for filenaming
+#'
+#' @param x a character string representing date/time as 'YYYY-MM-DD HH:MM:SS'
+#' @return character of format 'YYYYMMDD'
+
+
+format_date <- function(x){
+  x <- strptime(x = x, format = "%Y-%m-%d %T")
+  x <- format(x, format = "%Y%m%d")
+  return(as.character(x))
+}
