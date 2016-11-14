836a639e9abcd416e743c90b0e2b63dc5312a8e9
Sebastian Boeck <sebastian.boeck@boku.ac.at>
2016-11-14 14:13:06 +0100

    multiple polygons/points are now turned into a 'MultiPoint/Polzgon' type json geometry string, instead of a list

--- R/spat_to_jgeom.R
+++ R/spat_to_jgeom.R
@@ -4,35 +4,43 @@
 #'
 #' @param spat a spatial object of class SpatialPolygons or SpatialPoints
 #' @param round number of digits to round coordinates to
-#' @return list of json geometries, where length of the list is the number of
-#'  points or polygons in spat
+#' @return character string, a single json geometry
 #'
 
 spat_to_jgeom <- function(spat, round = Inf){
 
   rtrn <- vector(mode = 'list', length = length(spat))
 
+  if (any(grepl("SpatialPolygon", class(spat)))){
+    g_type <- "MultiPolygon"
+  } else if (any(grepl("SpatialPoints", class(spat)))){
+    g_type <- "MultiPoint"
+  }
+
   for (i in seq_along(spat)){
 
     poi_xy <- raster::geom(spat[i, ])[, c("x", "y"), drop = FALSE]
 
-    if (nrow(poi_xy) == 1 && "SpatialPoints" %in% methods::is(spat)){
+    if (g_type == "MultiPoint"){
 
       poi_xy <- data.frame(round(poi_xy, round))
-      g_type <- "Point"
       poi_xy <- with(poi_xy, paste(x, y, sep = ","))
+      poi_xy <- sprintf("[%s]", poi_xy)
 
-    } else if (nrow(poi_xy) > 1){
-
-      g_type <- "Polygon"
+    } else if (g_type == "MultiPolygon"){
 
       poi_xy <- data.frame(apply(round(poi_xy, round), 2, as.character))
       poi_xy <- with(poi_xy, paste(sprintf("[%s,%s]", x, y), collapse=","))
-      poi_xy <- sprintf("[%s]", poi_xy)
+      poi_xy <- sprintf("[[%s]]", poi_xy)
     }
 
-    rtrn[[i]] <- sprintf('{"type":"%s","coordinates":[%s]}',
-                         g_type, poi_xy)
+    rtrn[[i]] <- poi_xy
+
   }
+
+  rtrn <- paste(unlist(rtrn), collapse=",")
+  rtrn <- sprintf('{"type":"%s","coordinates":[%s]}', g_type, rtrn)
   return(rtrn)
 }
+
+
