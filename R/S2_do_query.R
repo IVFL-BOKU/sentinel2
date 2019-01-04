f1d9701bc804a26ffc565f69b8cb6989dcb35a7a
Mateusz Żółtak <zozlak@zozlak.org>
2019-01-04 18:45:24 +0100

    S2_do_query() improvements making debugging easier.

--- R/S2_do_query.R
+++ R/S2_do_query.R
@@ -4,24 +4,36 @@
 #'
 #' @param query list of named arguments 'known' to the database. Typically
 #'   created using e.g. 'S2_query_granule'
-#' @param path character altering the query url with respect to the desired
-#'   output. One of "product", "granule", "image", "qiData", "angle", "roi",
-#'   "job", "rgb".
-
-
-S2_do_query = function(query, path){
+#' @param path name of the rest API endpoint like "product", "granule", "image",
+#'   "qiData", "angle", "roi", "job" or "rgb".
+#' @param baseUrl base REST API URL
+S2_do_query = function(query, path, baseUrl = 'https://s2.boku.eodc.eu'){
   credentials = get_credentials()
 
   filter        = sapply(query, is.logical)
   query[filter] = as.numeric(query[filter])
 
   rtrn = httr::GET(
-    'https://s2.boku.eodc.eu',
+    baseUrl,
     config = httr::authenticate(credentials['user'], credentials['password']),
     path   = path,
     query  = query
   )
-  rtrn = as.data.frame(jsonlite::fromJSON(httr::content(rtrn, as = 'text')))
+  if (!httr::status_code(rtrn) %in% c(200, 204)) {
+    stop(
+      httr::modify_url(baseUrl, path = path, query = query),
+      ' failed with code ', httr::status_code(rtrn)
+    )
+  }
+  rtrn = tryCatch(
+    as.data.frame(jsonlite::fromJSON(httr::content(rtrn, as = 'text'))),
+    error = function(e) {
+      stop(
+        'failed to parse as JOSN results of ',
+        httr::modify_url(baseUrl, path = path, query = query)
+      )
+    }
+  )
 
   return(rtrn)
 }
